ef6885f9739fe4bcc4efb1a86b8ab56b45ea8341
donmaleek <bamboloshari@gmail.com>
2024-03-30 23:34:49 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then echo "Usage: $0 <URL>"; exit 1; fi
-echo "Size of the response body: $(curl -sS --output /dev/null --write-out "%{size_download}" "$1") bytes"
+echo "$(curl -sS --output /dev/null --write-out "%{size_download}" "$1")"
 
 
 
